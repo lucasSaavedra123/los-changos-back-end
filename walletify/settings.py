5996be795903770b479114e796749a001c38363f
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2022-08-27 18:39:49 -0300

    some changes

--- walletify/settings.py
+++ walletify/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j3r3l#6wm1-dh-e(torhql7mkcw+&y3q+wp*0^*m8asguklxzy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
