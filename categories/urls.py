d9b155b5ad9c7e32d401bf94a94270e2324005ba
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2022-10-01 22:38:34 -0300

    updated

--- categories/urls.py
+++ categories/urls.py
@@ -6,9 +6,11 @@ urlpatterns = [
     path('', category)
 ]
 
+"""
 if Category.objects.all().count() == 0:
     Category.objects.create(name="Impuestos y Servicios", material_ui_icon_name="AccountBalance")
     Category.objects.create(name="Entretenimiento y Ocio", material_ui_icon_name="Casino")
     Category.objects.create(name="Hogar y Mercado", material_ui_icon_name="Home")
     Category.objects.create(name="Buen vivir/Antojos", material_ui_icon_name="EmojiEmotions")
     Category.objects.create(name="Electrodomesticos", material_ui_icon_name="Kitchen")
+"""
