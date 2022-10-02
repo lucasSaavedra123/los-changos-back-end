76e21fa612a232e064561bf0f435dda51b7c2e09
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2022-10-02 17:48:30 -0300

    django_psdb_engine added

--- categories/models.py
+++ categories/models.py
@@ -6,6 +6,6 @@ from users.models import User
 # Create your models here.
 class Category(models.Model):
     id=models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, null=False, blank=False, default="Categoria Personalizada")
     material_ui_icon_name = models.CharField(max_length=50)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True, blank=True, db_constraint=False)
--- walletify/settings.py
+++ walletify/settings.py
@@ -93,10 +93,22 @@ if os.environ.get('ENVIRONMENT') == "DEV":
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-else:
+elif os.environ.get('ENVIRONMENT') == "CI":
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ.get('DB_NAME'),
+            'HOST': os.environ.get('DB_HOST'),
+            'PORT': os.environ.get('DB_PORT'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'OPTIONS': {'ssl': {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}}
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django_psdb_engine',
             'NAME': os.environ.get('DB_NAME'),
             'HOST': os.environ.get('DB_HOST'),
             'PORT': os.environ.get('DB_PORT'),
