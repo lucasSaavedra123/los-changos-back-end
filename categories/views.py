5715e439910e7582f9b7191c9b97aac785982f37
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2022-10-01 23:08:53 -0300

    Updated

--- categories/views.py
+++ categories/views.py
@@ -13,8 +13,9 @@ from categories.models import Category
 
 
 # Create your views here.
-@api_view(['GET', 'POST', 'DELETE'])
+@api_view(['GET', 'POST', 'PATCH'])
 def category(request):
+    request_body = json.loads(request.body.decode('utf-8'))
 
     if request.method == 'GET':
         static_categories = Category.objects.filter(user=None)
@@ -22,3 +23,20 @@ def category(request):
         all_categories = static_categories.union(user_categories)
         response_object = json.loads(serialize("json", all_categories))
         return JsonResponse(response_object, safe=False)
+
+    elif request.method == 'POST':
+        Category.objects.create(
+            user=request.META['user'],
+            material_ui_icon_name=request_body['material_ui_icon_name'],
+            name=request_body['name']
+        )
+
+        return Response(None, status=status.HTTP_201_CREATED)
+
+    elif request.method == 'PATCH':
+        category = Category.objects.get(id=request_body['id'])
+        category.name = request_body['name']
+        category.material_ui_icon_name = request_body['material_ui_icon_name']
+        category.save()
+
+        return Response(None, status=status.HTTP_200_OK)
