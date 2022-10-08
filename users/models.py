53e1ab230cde767bff39c1fe333d609723c37a0e
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2022-10-07 21:43:40 -0300

    fixed

--- users/models.py
+++ users/models.py
@@ -7,7 +7,4 @@ from users.validators import FirebaseUIDValidator
 # Create your models here.
 class User(models.Model):
     id=models.AutoField(primary_key=True)
-    firebase_uid = models.CharField(max_length=28, unique=True, validators=[
-        MinLengthValidator(28),
-        FirebaseUIDValidator,
-        ]) #UID is 28 characters long
+    firebase_uid = models.CharField(max_length=28, unique=True, validators=[MinLengthValidator(28)]) #UID is 28 characters long
