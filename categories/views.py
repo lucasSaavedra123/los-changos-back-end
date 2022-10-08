f320e6f7fae717ed438feb8a936973924df31b5f
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2022-10-08 17:11:03 -0300

    comments deleted

--- categories/views.py
+++ categories/views.py
@@ -58,7 +58,3 @@ def category(request):
             return Response(None, status=status.HTTP_200_OK)
     except KeyError as key_error_exception:
         return Response({"message": f"{key_error_exception} was not provided"}, status=status.HTTP_400_BAD_REQUEST)
-    """
-    except:
-        return Response({f"Server could not have handled request. Contact support team."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    """
--- expenses/views.py
+++ expenses/views.py
@@ -65,7 +65,3 @@ def expense(request):
             return Response(None, status=status.HTTP_200_OK)
     except KeyError as key_error_exception:
         return Response({"message": f"{key_error_exception} was not provided"}, status=status.HTTP_400_BAD_REQUEST)
-    """
-    except:
-        return Response({"message": "Server could not have handled request. Contact support team."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    """
