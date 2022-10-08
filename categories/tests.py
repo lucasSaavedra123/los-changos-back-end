c2a385208b86dd021a13f33a35b1b871e7d61468
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2022-10-08 13:20:06 -0300

    Test and Constants added

--- categories/tests.py
+++ categories/tests.py
@@ -3,6 +3,7 @@ from categories.models import Category
 from rest_framework.test import APITestCase, APIClient
 from django.urls import reverse
 from rest_framework import status
+from users.constants import FIREBASE_UID_LENGTH
 from utils import create_random_string
 from users.models import User
 from django.core.exceptions import ValidationError
@@ -12,7 +13,7 @@ from django.core.exceptions import ValidationError
 class TestCategoriesModel(TestCase):
 
     def setUp(self):
-        self.a_user = User.objects.create(firebase_uid=create_random_string(28))
+        self.a_user = User.objects.create(firebase_uid=create_random_string(FIREBASE_UID_LENGTH))
         self.category_created = Category.objects.create(user=self.a_user, name="Education", material_ui_icon_name="School")
 
     def test_category_is_created_for_user(self):
@@ -22,10 +23,20 @@ class TestCategoriesModel(TestCase):
         self.category_created.delete()
         self.assertEqual(len(Category.categories_from_user(self.a_user)), 5)
 
+    def test_created_category_is_not_static(self):
+        self.assertFalse(self.category_created.static)
+
     def test_two_categories_with_same_name_should_not_be_created_for_a_user(self):
         with self.assertRaises(ValidationError):
             Category.objects.create(user=self.a_user, name="eDucatioN", material_ui_icon_name="Car")
 
+    def test_category_dictionary_serialization(self):
+        self.assertDictEqual(self.category_created.as_dict,{
+                "id": self.category_created.id,
+                "name": "education",
+                "material_ui_icon_name": "School",
+                "static": False
+            })
 
 """
 class TestCategoriesView(APITestCase):
--- users/models.py
+++ users/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
 
+from users.constants import FIREBASE_UID_LENGTH
+
 
 # Create your models here.
 class User(models.Model):
     id=models.AutoField(primary_key=True)
-    firebase_uid = models.CharField(max_length=28, unique=True, validators=[MinLengthValidator(28)]) #UID is 28 characters long
+    firebase_uid = models.CharField(max_length=FIREBASE_UID_LENGTH, unique=True, validators=[MinLengthValidator(FIREBASE_UID_LENGTH)]) #UID is 28 characters long
