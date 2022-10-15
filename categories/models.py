79f81f8c78ea7de6916c12a74e1c0f173a12a2bf
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2022-10-15 18:59:22 -0300

    update

--- categories/models.py
+++ categories/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
 
 from users.models import User
+from utils import create_random_color_string
 
 
 # Create your models here.
@@ -18,6 +20,14 @@ class Category(models.Model):
         max_length=50,
         default="Paid",
         editable=True)
+
+    color = models.CharField(
+        max_length=50,
+        default=None,
+        editable=False,
+        null=True
+    )
+
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -63,6 +73,8 @@ class Category(models.Model):
                 if category.name == self.name:
                     raise ValidationError(
                         "User cannot create another repeated category")
+
+        self.color = create_random_color_string()
 
         self.full_clean()
         super(Category, self).save(*args, **kwargs)
--- utils.py
+++ utils.py
@@ -7,3 +7,9 @@ def create_random_string(string_length):
     return ''.join(
         random.choice(string.ascii_uppercase + string.ascii_lowercase) for i in range(string_length)
     )
+
+def create_random_color_string():
+    R = random.randint(0,255)
+    G = random.randint(0,255)
+    B = random.randint(0,255)
+    return f"rgba({R},{G},{B},0.2)"
