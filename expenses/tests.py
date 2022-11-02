282dd31fcebec34e801d0edee915a95b0ffa4d2f
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2022-11-01 22:52:43 -0300

    Test Fixed

--- expenses/tests.py
+++ expenses/tests.py
@@ -16,6 +16,11 @@ from datetime import date
 
 # Create your tests here.
 class TestExpensesModel(TestCase):
+    def assertActionInSecureEnvironment(self, action):
+        os.environ["ENVIRONMENT"] = "PROD"
+        self.assertEqual(action(self).status_code, status.HTTP_401_UNAUTHORIZED)
+        os.environ["ENVIRONMENT"] = "DEV"
+
     def setUp(self):
         self.a_user = User.objects.create(
             firebase_uid=create_random_string(FIREBASE_UID_LENGTH))
@@ -37,7 +42,7 @@ class TestExpensesModel(TestCase):
         self.assertEqual(len(Expense.expenses_from_user(self.a_user)), 0)
 
     def test_future_expenses_cannot_be_created(self):
-        with self.assertRaises(ValidationError):
+        with self.assertRaisesMessage(ValidationError, "{'date': ['Expense date cannot be in the future.']}"):
             Expense.objects.create(
                 user=self.a_user,
                 value=1500,
@@ -47,7 +52,7 @@ class TestExpensesModel(TestCase):
             )
 
     def test_negative_expenses_cannot_be_created(self):
-        with self.assertRaises(ValidationError):
+        with self.assertRaisesMessage(ValidationError, "{'value': ['Ensure this value is greater than or equal to 0.01.']}"):
             Expense.objects.create(
                 user=self.a_user,
                 value=-58.25,
@@ -206,6 +211,7 @@ class TestExpensesModel(TestCase):
         self.assertEqual(filtered_expenses[1], another_expense_created)
         self.assertEqual(filtered_expenses[2], old_expense)
 
+
 class TestExponsesView(APITestCase):
     def setUp(self):
         self.endpoint = '/expense'
@@ -229,8 +235,7 @@ class TestExponsesView(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        response = self.client.delete(
-            self.endpoint, {'id': '1'}, format='json')
+        response = self.client.delete(self.endpoint, {'id': '1'}, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -281,15 +286,12 @@ class TestExponsesView(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        expected_response = {
-            'id': 1,
-            'value': 59999.0,
-            'date': '2021-03-25',
-            'category': {'id': 2, 'material_ui_icon_name': 'Casino', 'name': 'Entretenimiento Y Ocio', 'static': True},
-            'name': 'Expensive Expense'
-        }
+        json_response = response.json()[0]
 
-        self.assertDictEqual(response.json()[0], expected_response)
+        self.assertEqual(json_response['value'], 59999.0)
+        self.assertEqual(json_response['date'], '2021-03-25')
+        self.assertEqual(json_response['category']['id'], 2)
+        self.assertEqual(json_response['name'], 'Expensive Expense')
 
     def test_user_update_all_information_related_to_category_expense(self):
         self.client.post(self.endpoint, {
@@ -311,19 +313,12 @@ class TestExponsesView(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        expected_response = {
-            'id': 1,
-            'value': 87444.0,
-            'date': '2020-02-05',
-            'category': {
-                'id': 1,
-                'material_ui_icon_name': 'AccountBalance',
-                'name': 'Impuestos Y Servicios',
-                'static': True},
-            'name': "Modified Expense"
-        }
+        json_response = response.json()[0]
 
-        self.assertDictEqual(response.json()[0], expected_response)
+        self.assertEqual(json_response['value'], 87444.0)
+        self.assertEqual(json_response['date'], '2020-02-05')
+        self.assertEqual(json_response['category']['id'], 1)
+        self.assertEqual(json_response['name'], "Modified Expense")
 
     def test_user_update_only_category_of_created_expense(self):
         self.client.post(self.endpoint, {
@@ -345,20 +340,12 @@ class TestExponsesView(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        expected_response = {
-            'id': 1,
-            'value': 250.5,
-            'date': '2021-01-30',
-            'category': {
-                'id': 1,
-                'material_ui_icon_name': 'AccountBalance',
-                'name': 'Impuestos Y Servicios',
-                'static': True
-            },
-            'name': "Another expense"
-        }
+        json_response = response.json()[0]
 
-        self.assertDictEqual(response.json()[0], expected_response)
+        self.assertEqual(json_response['value'], 250.5)
+        self.assertEqual(json_response['date'], '2021-01-30')
+        self.assertEqual(json_response['category']['id'], 1)
+        self.assertEqual(json_response['name'], "Another expense")
 
     def test_user_forgots_to_include_field_in_create_request(self):
         response = self.client.post(self.endpoint, {
@@ -396,18 +383,7 @@ class TestExponsesView(APITestCase):
             "timeline": ["2022-06-02", "2022-06-02"]
         }, format='json')
 
-        self.assertDictEqual(response.json()[0], {
-            'id': 2,
-            "value": 123456,
-            'date': '2022-06-02',
-            'category': {
-                'id': 3,
-                'material_ui_icon_name': 'Home',
-                'name': 'Hogar Y Mercado',
-                'static': True
-            },
-            "name":'Another expense'
-        })
+        self.assertEqual(response.json()[0]['category']['id'], 3)
 
     def test_user_reads_expenses_within_extended_time_line(self):
         self.client.post(self.endpoint, {
@@ -428,31 +404,18 @@ class TestExponsesView(APITestCase):
             "timeline": ["2021-01-01", "2023-01-01"]
         }, format='json')
 
-        self.assertDictEqual(response.json()[0], {
-            "id": 1,
-            "value": 10599.0,
-            'date': '2021-05-20',
-            'category': {
-                'id': 1,
-                'material_ui_icon_name': 'AccountBalance',
-                'name': 'Impuestos Y Servicios',
-                'static': True
-            },
-            "name":'Very old expense'
-        })    
+        json_response_one = response.json()[0]
+        json_response_two = response.json()[1]
 
-        self.assertDictEqual(response.json()[1], {
-            "id": 2,
-            "value": 123456.0,
-            'date': '2022-06-02',
-            'category': {
-                'id': 3,
-                'material_ui_icon_name': 'Home',
-                'name': 'Hogar Y Mercado',
-                'static': True
-            },
-            "name":'Another expense'
-        })        
+        self.assertEqual(json_response_one['value'], 123456.0)
+        self.assertEqual(json_response_one['date'], '2022-06-02')
+        self.assertEqual(json_response_one['category']['id'], 3)
+        self.assertEqual(json_response_one['name'], "Another expense")
+
+        self.assertEqual(json_response_two['value'], 10599.0)
+        self.assertEqual(json_response_two['date'], '2021-05-20')
+        self.assertEqual(json_response_two['category']['id'], 1)
+        self.assertEqual(json_response_two['name'], 'Very old expense')
 
     def test_user_reads_expenses_within_extended_time_line_of_a_specific_category(self):
         self.client.post(self.endpoint, {
@@ -463,7 +426,7 @@ class TestExponsesView(APITestCase):
         }, format='json')
 
         self.client.post(self.endpoint, {
-            'value': 10599,
+            'value': 10500,
             'date': '2021-12-30',
             'category_id': 1,
             'name': "Same category very old expense"
@@ -483,32 +446,18 @@ class TestExponsesView(APITestCase):
 
         self.assertEqual(len(response.json()), 2)
 
-        self.assertDictEqual(response.json()[0], {
-            "id": 1,
-            "value": 10599.0,
-            'date': '2021-05-20',
-            'category': {
-                'id': 1,
-                'material_ui_icon_name': 'AccountBalance',
-                'name': 'Impuestos Y Servicios',
-                'static': True
-            },
-            "name":'Very old expense'
-        })
+        json_response_one = response.json()[0]
+        json_response_two = response.json()[1]
 
-        self.assertDictEqual(response.json()[1], {
-            "id": 2,
-            "value": 10599,
-            'date': '2021-12-30',
-            'category': {
-                'id': 1,
-                'material_ui_icon_name': 'AccountBalance',
-                'name': 'Impuestos Y Servicios',
-                'static': True
-            },
-            "name":'Same category very old expense'
-        })       
+        self.assertEqual(json_response_one['value'], 10500.0)
+        self.assertEqual(json_response_one['date'], '2021-12-30')
+        self.assertEqual(json_response_one['category']['id'], 1)
+        self.assertEqual(json_response_one['name'], 'Same category very old expense')
 
+        self.assertEqual(json_response_two['value'], 10599.0)
+        self.assertEqual(json_response_two['date'], '2021-05-20')
+        self.assertEqual(json_response_two['category']['id'], 1)
+        self.assertEqual(json_response_two['name'], "Very old expense")
 
     def test_user_has_no_provide_valid_token_to_read_expenses(self):
         os.environ["ENVIRONMENT"] = "PROD"
