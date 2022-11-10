b87073149add5355ec7669536b869af1590a9910
Lucas A. Saavedra <46687572+lucasSaavedra123@users.noreply.github.com>
2022-11-10 18:17:51 -0300

    Update urls.py

--- walletify/urls.py
+++ walletify/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.urls import re_path
 
 from rest_framework import permissions
@@ -58,8 +58,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
