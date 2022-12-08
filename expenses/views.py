a1edba3268503ddb2165ef380d16bf73b3467940
nachotornati <nacho.tornati@gmail.com>
2022-12-07 21:39:56 -0300

    validations

--- expenses/views.py
+++ expenses/views.py
@@ -31,39 +31,54 @@ def expense(request):
             return JsonResponse(expenses_as_dict, safe=False)
 
         elif request.method == 'POST':
-            category = Category.objects.get(id=request_body['category_id'])
-
-            if request_body['type'] == 'transfer_send':
-                Expense.create_expense_for_user(
-                request.META['user'],
-                value=request_body['value'],
-                category=category,
-                date=request_body['date'],
-                name="Transferencia enviada",
-                type=request_body['type']
-                )
-
+            user_expenses = Expense.expenses_from_user(request.META['user'])
+            suma = 0
+            for userExpense in user_expenses:
+                if userExpense.type == 'income':
+                    suma += userExpense.value
+                elif userExpense.type == 'expense':
+                    suma -= userExpense.value
+                elif userExpense.type == 'transfer_send':
+                    suma -= userExpense.value
+                elif userExpense.type == 'transfer_receive':
+                    suma += userExpense.value
+            print(suma - int(request_body['value']))
+            if (suma - int(request_body['value'])>=0 or request_body['type'] == 'income'):
                 
-                user = User.objects.get(alias=request_body['alias'])
-                income_category = Category.objects.get(id=6)
-                
-                Expense.create_expense_for_user(
-                user,
-                value=request_body['value'],
-                category=income_category,
-                date=request_body['date'],
-                name="Transferencia recibida",
-                type="transfer_receive"
-                )
-            else:   
-                Expense.create_expense_for_user(
+                category = Category.objects.get(id=request_body['category_id'])
+
+                if request_body['type'] == 'transfer_send':
+                    userToReceive = User.objects.get(alias=request_body['alias'])
+                    income_category = Category.objects.get(id=6)
+                    print("HOLAAAAAAAAAA")
+                    print(userToReceive)
+                    Expense.create_expense_for_user(
+                    userToReceive,
+                    value=request_body['value'],
+                    category=income_category,
+                    date=request_body['date'],
+                    name="Transferencia recibida",
+                    type="transfer_receive"
+                    )
+
+                    Expense.create_expense_for_user(
                     request.META['user'],
                     value=request_body['value'],
                     category=category,
                     date=request_body['date'],
-                    name=request_body['name'],
-                    type=request_body['type'],
-                )
+                    name="Transferencia enviada",
+                    type=request_body['type']
+                    )
+
+                else:   
+                    Expense.create_expense_for_user(
+                        request.META['user'],
+                        value=request_body['value'],
+                        category=category,
+                        date=request_body['date'],
+                        name=request_body['name'],
+                        type=request_body['type'],
+                    )
 
             return Response(None, status=status.HTTP_201_CREATED)
 
@@ -77,20 +92,33 @@ def expense(request):
             return Response(None, status=status.HTTP_200_OK)
 
         elif request.method == 'PATCH':
+            all_user_expenses = Expense.expenses_from_user(request.META['user'])
             expense = Expense.objects.get(id=request_body['id'])
 
             if expense.user != request.META['user']:
                 return Response(None, status=status.HTTP_403_FORBIDDEN)
+            
+            income = 0
+            for e in all_user_expenses:
+                if e.type == 'income':
+                    income += e.value
+                elif e.type == 'expense':
+                    income -= e.value
+                elif e.type == 'transfer_send':
+                    income -= e.value
+                elif e.type == 'transfer_receive':
+                    income += e.value
 
-            expense.name = request_body['name']
-            expense.value = request_body['value']
-            expense.date = request_body['date']
-            expense.category = Category.objects.get(
-                id=request_body['category_id'])
+            if((expense.type == 'income' and (income - int(expense.value) + int(request_body['value']) >= 0)) or (expense.type == 'expense' and (income + int(expense.value) - int(request_body['value']) >= 0))):
+                expense.name = request_body['name']
+                expense.value = request_body['value']
+                expense.date = request_body['date']
+                expense.category = Category.objects.get(
+                    id=request_body['category_id'])
 
-            expense.save()
+                expense.save()
 
-            return Response(None, status=status.HTTP_200_OK)
+                return Response(None, status=status.HTTP_200_OK)
     except KeyError as key_error_exception:
         return Response({"message": f"{key_error_exception} was not provided"}, status=status.HTTP_400_BAD_REQUEST)
 
