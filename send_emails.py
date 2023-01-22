d3f1628cb6980d456d661725fbe3efa09b627f67
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-01-22 13:30:34 -0300

    send_emails.py updated

--- send_emails.py
+++ send_emails.py
@@ -1,12 +1,23 @@
-"""
 from django.core.mail import send_mail
 
-
+"""
 send_mail(
     'Subject here',
     'Here is the message.',
-    'fwefwefwe@wefwefew.com',
-    ['lucasarielsaavedra@hotmail.com'],
+    'from@example.com',
+    ['to@example.com'],
     fail_silently=False,
 )
+
+@periodic_task(
+run_every=(crontab(hour=3, minute=34)), #runs exactly at 3:34am every day
+name="Dispatch_scheduled_mail",
+reject_on_worker_lost=True,
+ignore_result=True)
+def schedule_mail():
+    message = render_to_string('app/schedule_mail.html')
+    mail_subject = 'Scheduled Email'
+    to_email = getmail
+    email = EmailMessage(mail_subject, message, to=[to_email])
+    email.send()
 """
