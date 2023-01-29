12fc48a1097fc15ea6d40944afa9d181cbfc6636
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-01-29 18:37:29 -0300

    Code updated

--- walletify/tasks.py
+++ walletify/tasks.py
@@ -4,19 +4,31 @@ from django.core.mail import send_mail
 
 def notify_expiration_expenses():
     from budgets.models import FutureExpenseDetail
-    print("Sending Emails!!!")
-    string_email = "¡ACUERDATE DE PAGAR!" + '\n\n' + "Recuerda que se vence el siguientes pagos: "
+    from budgets.models import Budget
+    from users.models import User
 
-    for future_expense_detail in FutureExpenseDetail.objects.all():
+    for user in User.objects.all():
 
-        if future_expense_detail.should_be_notified():
-            string_email += '\n\n' + "-Nombre de gasto futuro:" + future_expense_detail.name + '\n\n' + 'Valor: ' + str(future_expense_detail.value) + '\n\n' + 'Fecha de vencimiento: ' + str(future_expense_detail.expiration_date) + '\n\n' + 'Categoría: ' + future_expense_detail.category.name
+        current_budget = Budget.current_budget_of(user)
+
+        if current_budget is not None:
+            future_expenses = FutureExpenseDetail.objects.filter(assigned_budget=current_budget)
+            future_expenses = [detail for detail in future_expenses if detail.should_be_notified()]
+
+            if len(future_expenses) > 0:
+
+                string_email = "¡ACUERDATE DE PAGAR!" + '\n\n' + "Recuerda que se vence el siguientes pagos: "
+
+                for future_expense_detail in FutureExpenseDetail.objects.all():
+
+                    if future_expense_detail.should_be_notified():
+                        string_email += '\n\n' + "-Nombre de gasto futuro:" + future_expense_detail.name + '\n\n' + 'Valor: ' + str(future_expense_detail.value) + '\n\n' + 'Fecha de vencimiento: ' + str(future_expense_detail.expiration_date) + '\n\n' + 'Categoría: ' + future_expense_detail.category.name
 
 
-    send_mail(
-        '[IMPORTANTE] ¡ACUERDATE DE PAGAR!',
-        string_email + '\n\n\n\n' + '¡Gracias por usar Walletify!',
-        'notifications@walletify.com',
-        [future_expense_detail.assigned_budget.user.email],
-        fail_silently=False,
-    )
+                send_mail(
+                    '[IMPORTANTE] ¡ACUERDATE DE PAGAR!',
+                    string_email + '\n\n\n\n' + '¡Gracias por usar Walletify!',
+                    'notifications@walletify.com',
+                    [future_expense_detail.assigned_budget.user.email],
+                    fail_silently=False,
+                )
