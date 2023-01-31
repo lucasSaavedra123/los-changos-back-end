619c51767b9198bc5c3ada4f50478dd01b252b86
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-01-30 21:40:43 -0300

    Code Deleted

--- budgets/views.py
+++ budgets/views.py
@@ -62,97 +62,94 @@ def budget(request):
         if datetime.strptime(request_body['initial_date'], '%Y-%d-%m') > datetime.strptime(request_body['final_date'], '%Y-%d-%m'):
             return Response({"message": f"Initial date cannot be greater than final date"}, status=status.HTTP_400_BAD_REQUEST)
         
-    try:
-        if request.method == 'GET':
-            user_budgets = Budget.all_from_user(request.META['user'])
-            budgets_as_dict = [budget.as_dict for budget in user_budgets]
-            all_categories_from_user = Category.categories_from_user(request.META['user'])
+    if request.method == 'GET':
+        user_budgets = Budget.all_from_user(request.META['user'])
+        budgets_as_dict = [budget.as_dict for budget in user_budgets]
+        all_categories_from_user = Category.categories_from_user(request.META['user'])
 
-            # Front-End requires all limits with each category, even if it's 0
-            for budget_index in range(len(budgets_as_dict)):
-                categories_that_have_limit_budgets = [ detail['category']['id'] for detail in budgets_as_dict[budget_index]['details'] if detail.get('limit', None) is not None ]
-                budgets_as_dict[budget_index]['details'] += [{ 'category': user_category.as_dict, 'limit': 0, 'spent': 0 } for user_category in all_categories_from_user if user_category.id not in categories_that_have_limit_budgets]
+        # Front-End requires all limits with each category, even if it's 0
+        for budget_index in range(len(budgets_as_dict)):
+            categories_that_have_limit_budgets = [ detail['category']['id'] for detail in budgets_as_dict[budget_index]['details'] if detail.get('limit', None) is not None ]
+            budgets_as_dict[budget_index]['details'] += [{ 'category': user_category.as_dict, 'limit': 0, 'spent': 0 } for user_category in all_categories_from_user if user_category.id not in categories_that_have_limit_budgets]
 
-            return JsonResponse(budgets_as_dict, safe=False)
+        return JsonResponse(budgets_as_dict, safe=False)
 
-        elif request.method == 'POST':
-            new_budget = Budget.objects.create(
-                user=request.META['user'],
-                initial_date=request_body['initial_date'],
-                final_date=request_body['final_date']
-            )
+    elif request.method == 'POST':
+        new_budget = Budget.objects.create(
+            user=request.META['user'],
+            initial_date=request_body['initial_date'],
+            final_date=request_body['final_date']
+        )
 
+        try:
+            for detail in request_body['details']:
+                if 'limit' in detail:
+                    if detail['limit'] > 0:
+                        new_budget.add_limit(Category.objects.get(id=detail['category_id']), detail['limit'])
+                elif 'value' in detail:
+                    if detail['value'] > 0:
+                        new_budget.add_future_expense(Category.objects.get(id=detail['category_id']), detail['value'], detail['name'], detail['expiration_date'])                        
+                else:
+                    new_budget.delete()
+                    return Response({"message": f"Request should include in details field limit and value for limit or future expense detail respectively"}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            new_budget.delete()
+            return Response({"message": f"{e}"}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(None, status=status.HTTP_201_CREATED)
+
+    budget = Budget.objects.get(id=request_body['id'])
+
+    if request.method == 'DELETE':
+        budget = Budget.objects.get(id=request_body['id'])
+
+        if budget.user != request.META['user']:
+            return Response(None, status=status.HTTP_403_FORBIDDEN)
+
+        if not budget.active:
+            budget.delete()
+        else:
+            return Response({"message": f"Current budgets are neither editable and removable"}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(None, status=status.HTTP_200_OK)
+
+    if request.method == 'PATCH':
+        budget = Budget.objects.get(id=request_body['id'])
+
+        if budget.user != request.META['user']:
+            return Response(None, status=status.HTTP_403_FORBIDDEN)
+
+        if budget.active:
+            return Response({"message": f"Current budgets are neither editable and removable"}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            budget.initial_date = datetime.strptime(request_body['initial_date'], '%Y-%m-%d').date()
+            budget.final_date = datetime.strptime(request_body['final_date'], '%Y-%m-%d').date()
+            current_details = LimitDetail.from_budget(budget)
+
+            for detail in current_details:
+                detail.delete()
             try:
                 for detail in request_body['details']:
                     if 'limit' in detail:
                         if detail['limit'] > 0:
-                            new_budget.add_limit(Category.objects.get(id=detail['category_id']), detail['limit'])
+                            budget.add_limit(Category.objects.get(id=detail['category_id']), detail['limit'])
                     elif 'value' in detail:
                         if detail['value'] > 0:
-                            new_budget.add_future_expense(Category.objects.get(id=detail['category_id']), detail['value'], detail['name'], detail['expiration_date'])                        
+                            budget.add_future_expense(Category.objects.get(id=detail['category_id']), detail['value'], detail['name'], detail['expiration_date'])
                     else:
-                        new_budget.delete()
                         return Response({"message": f"Request should include in details field limit and value for limit or future expense detail respectively"}, status=status.HTTP_400_BAD_REQUEST)
             except Exception as e:
-                new_budget.delete()
-                return Response({"message": f"{e}"}, status=status.HTTP_400_BAD_REQUEST)
-
-            return Response(None, status=status.HTTP_201_CREATED)
-
-        budget = Budget.objects.get(id=request_body['id'])
-
-        if request.method == 'DELETE':
-            budget = Budget.objects.get(id=request_body['id'])
-
-            if budget.user != request.META['user']:
-                return Response(None, status=status.HTTP_403_FORBIDDEN)
-
-            if not budget.active:
-                budget.delete()
-            else:
-                return Response({"message": f"Current budgets are neither editable and removable"}, status=status.HTTP_400_BAD_REQUEST)
-
-            return Response(None, status=status.HTTP_200_OK)
-
-        if request.method == 'PATCH':
-            budget = Budget.objects.get(id=request_body['id'])
-
-            if budget.user != request.META['user']:
-                return Response(None, status=status.HTTP_403_FORBIDDEN)
-
-            if budget.active:
-                return Response({"message": f"Current budgets are neither editable and removable"}, status=status.HTTP_400_BAD_REQUEST)
-            else:
-                budget.initial_date = datetime.strptime(request_body['initial_date'], '%Y-%m-%d').date()
-                budget.final_date = datetime.strptime(request_body['final_date'], '%Y-%m-%d').date()
-                current_details = LimitDetail.from_budget(budget)
+                #If something failed, we recover the previous state
+                for detail in Detail.from_budget(budget):
+                    detail.delete()
 
                 for detail in current_details:
-                    detail.delete()
-                try:
-                    for detail in request_body['details']:
-                        if 'limit' in detail:
-                            if detail['limit'] > 0:
-                                budget.add_limit(Category.objects.get(id=detail['category_id']), detail['limit'])
-                        elif 'value' in detail:
-                            if detail['value'] > 0:
-                                budget.add_future_expense(Category.objects.get(id=detail['category_id']), detail['value'], detail['name'], detail['expiration_date'])
-                        else:
-                            return Response({"message": f"Request should include in details field limit and value for limit or future expense detail respectively"}, status=status.HTTP_400_BAD_REQUEST)
-                except Exception as e:
-                    #If something failed, we recover the previous state
-                    for detail in Detail.from_budget(budget):
-                        detail.delete()
+                    detail.save()
+                return Response({"message": f"{e}"}, status=status.HTTP_400_BAD_REQUEST)
 
-                    for detail in current_details:
-                        detail.save()
-                    return Response({"message": f"{e}"}, status=status.HTTP_400_BAD_REQUEST)
+            budget.save(update=True)
 
-                budget.save(update=True)
-
-            return Response(None, status=status.HTTP_200_OK)
-    except KeyError as key_error_exception:
-        return Response({"message": f"{key_error_exception} was not provided"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(None, status=status.HTTP_200_OK)
 
 
 @api_view(['GET'])
